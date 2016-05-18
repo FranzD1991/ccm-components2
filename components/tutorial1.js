9f56bef1b42113a08489771ec0d12ecb1f1caf42
Franz <fddahmann91@gmail.com>
2016-05-18 15:32:08 +0200

    Erstellung des Gerüstes

--- components/tutorial1.js
+++ components/tutorial1.js
@@ -5,16 +5,46 @@
 ccm.component({
     name: 'tutorial1',
     config: {
+        html:  [ ccm.store, { local: './json/tutorial.json' } ],
         key: 'test',
-        store:[ccm.store, { url: 'ws://ccm2.inf.h-brs.de/index.js', store: 'chat' } ]
+        store:[ccm.store, { url: 'ws://ccm2.inf.h-brs.de/index.js', store: 'chat' } ],
+        style: [ ccm.load, './css/tutorial.css' ],
+        user:  [ ccm.instance, 'http://kaul.inf.h-brs.de/ccm/components/user.js' ]
     },
     Instance: function () {
-        this.render=function(callback){
-            var element = ccm.helper.element( this );
-            this.store.get( this.key, function ( dataset ) {
-                if ( callback ) callback();
+        var self = this;
+        self.init = function ( callback ) {
+            self.store.onChange = function () { self.render(); };
+            callback();
+        };
+        self.render = function ( callback ) {
+            var element = ccm.helper.element( self );
+            self.store.get( self.key, function ( dataset ) {
+                if ( dataset === null )
+                    self.store.set( { key: self.key, messages: [] }, proceed );
+                else
+                    proceed( dataset );
+                function proceed( dataset ) {
+                    element.html( ccm.helper.html( self.html.get( 'main' ) ) );
+                    var messages_div = ccm.helper.find( self, '.messages' );
+                    for ( var i = 0; i < dataset.messages.length; i++ ) {
+                        messages_div.append( ccm.helper.html( self.html.get( 'message' ), {
+                            name: ccm.helper.val( message.user ),
+                            text: ccm.helper.val( message.text )
+                        }  ) );
+                    }
+                    messages_div.append( ccm.helper.html( self.html.get( 'input' ), {
+                        onsubmit: function () {
+                            var value = ccm.helper.val( ccm.helper.find( self, 'input' ).val().trim() );
+                            if ( value === '' ) return;
+                            dataset.messages.push( { user: self.user.data().key, text: value } );
+                            self.store.set( dataset, function () { self.render(); } );
+                            return false;
+                        }
+                    } ) );
+                    if ( callback ) callback();
+                }
             } );
-            if(callback) callback();
         }
     }
-});
+} );

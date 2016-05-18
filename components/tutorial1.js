7744cdaa6745a74a797c878350e4c5b8615f27f2
Franz <fddahmann91@gmail.com>
2016-05-18 11:48:13 +0200

    Erstellung des Gerüstes

--- components/tutorial1.js
+++ components/tutorial1.js
@@ -2,3 +2,12 @@
  * Created by Franz on 18.05.2016.
  */
 
+ccm.component({
+    name: 'tutorial1',
+    Instance: function () {
+        this.render=function(callback){
+            ccm.helper.element(this);
+            if(callback) callback();
+        }
+    }
+})

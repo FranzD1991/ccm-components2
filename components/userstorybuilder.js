3201472e2503f239786de47e5ba15ca654a85df8
Franz <fddahmann91@gmail.com>
2016-05-24 13:04:22 +0200

    Erstellung des Gerüstes

--- components/userstorybuilder.js
+++ components/userstorybuilder.js
@@ -10,6 +10,38 @@ ccm.component({
             ccm.helper.element(this);
             var element = ccm.helper.element( this );
             element.html( 'Hello, World!' );
+            
+            var n=0;
+            var us= [];
+
+            function Userstory(hl,des,prio,eff,va){
+                this.hl = hl;
+                this.des = des;
+                this.prio = prio;
+                this.eff = eff;
+                this.va =va;
+            }
+            
+            function testInsert(){
+                
+            }
+            
+            function submitUS() {
+                us[n]=userstory(1,2,3,4,5);
+                n++;
+            }
+
+            function deleteUS() {
+                
+            }
+            
+            function getUS() {
+                
+            }
+
+            function showallUS() {
+
+            }
             if(callback) callback();
         }
     }
